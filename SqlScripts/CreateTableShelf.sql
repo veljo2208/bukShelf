045c9054060709c77bbec9ec5bc37de22583a8cd
Veljko Topalovic <veljko.topalovic@dex.rs>
2023-12-25 16:30:22 +0100

    Removed comment

--- SqlScripts/CreateTableShelf.sql
+++ SqlScripts/CreateTableShelf.sql
@@ -4,7 +4,7 @@
     Surface DECIMAL NOT NULL,
     Book_Count INT NOT NULL,
     Current_Weight_Load DECIMAL NOT NULL,
-    Material VARCHAR(10) NOT NULL, -- Change the size according to your longest enum value
+    Material VARCHAR(10) NOT NULL,
     Shelf_Status VARCHAR(20) NOT NULL
 );
 
